678e7173ac13257603a7e360fa9ebe52d04de6d4
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-03-02 20:48:28 +0900

    taskコントローラの編集

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,28 +1,22 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-  # GET /tasks
   def index
-    @tasks = Task.all
+    @tasks = Task.all.order('created_at DESC')
   end
 
-  # GET /tasks/1
   def show
   end
 
-  # GET /tasks/new
   def new
     @task = Task.new
   end
 
-  # GET /tasks/1/edit
   def edit
   end
 
-  # POST /tasks
   def create
     @task = Task.new(task_params)
-
     if @task.save
       redirect_to @task, notice: 'Task was successfully created.'
     else
@@ -30,7 +24,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tasks/1
   def update
     if @task.update(task_params)
       redirect_to @task, notice: 'Task was successfully updated.'
@@ -39,20 +32,17 @@ class TasksController < ApplicationController
     end
   end
 
-  # DELETE /tasks/1
   def destroy
     @task.destroy
     redirect_to tasks_url, notice: 'Task was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_task
       @task = Task.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def task_params
-      params.require(:task).permit(:title, :content, :deadline, :priority, :status, :user_id)
+      params.require(:task).permit(:title, :content, :deadline, :priority, :user_id)
     end
 end
