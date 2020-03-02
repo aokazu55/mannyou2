c3189106b63789e522d683d9d18338c9a77e53a2
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-03-02 21:13:45 +0900

    バリデーション設定実行

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
+RSpec.describe 'タスク管理機能', type: :model do
 
-RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'titleが空ならバリデーションが通らない' do
+    task = Task.new(title: '', content: '失敗テスト', deadline: '失敗テスト', priority: '失敗テスト')
+    expect(task).not_to be_valid
+  end
+
+  it 'contentが空ならバリデーションが通らない' do
+    task = Task.new(title: '失敗テスト２', content: '', deadline: '失敗テスト２', priority: '失敗テスト２')
+    expect(task).not_to be_valid
+  end
+
+  it 'deadlineが空ならバリデーションが通らない' do
+    task = Task.new(title: '失敗テスト３', content: '失敗テスト３', deadline: '', priority: '失敗テスト３')
+    expect(task).not_to be_valid
+  end
+
+  it 'priorityが空ならバリデーションが通らない' do
+    task = Task.new(title: '失敗テスト４', content: '失敗テスト４', deadline: '失敗テスト４', priority: '')
+    expect(task).not_to be_valid
+  end
+
+  it '入力欄全てに内容が記載されていればバリデーションが通る' do
+    # ここに内容を記載する
+    task = Task.new(title: '成功テスト', content: '成功テスト', deadline: '成功テスト', priority: '成功テスト')
+    expect(task).to be_valid
+  end
 end
