28c16d0a11c28ae6446fcfaa01de3e8b3ea2c240
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-03-02 20:57:51 +0900

    システムスペックの記述

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,10 +1,5 @@
 FactoryBot.define do
   factory :task do
-    title { "MyString" }
-    content { "MyText" }
-    deadline { "2020-03-02" }
-    priority { "MyString" }
-    status { "MyString" }
-    user_id { 1 }
+    
   end
 end
