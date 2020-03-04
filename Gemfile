d7201b42a4536724f9fafa02fefc2fc140d5099c
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-03-04 15:15:42 +0900

    Kaminari・bootstrap・jquery-railsのインストール実施

--- Gemfile
+++ Gemfile
@@ -10,6 +10,9 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'msgpack'
+gem 'kaminari'
+gem 'bootstrap'
+gem 'jquery-rails'
 
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,22 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+PER = 7
 
   def index
     if params[:sort_deadline]
-      @tasks = Task.all.order(deadline: :asc)
+      @tasks = Task.all.order(deadline: :asc).page(params[:page]).per(7)
     elsif params[:sort_priority]
       @tasks = Task.all.order(priority: :desc)
+    elsif params[:title].blank? && params[:status].blank?
+      @tasks = Task.all.page(params[:page])
     elsif params[:title].blank? && params[:status]
-      @tasks = Task.where('status LIKE ?', "%#{params[:status]}%")
-    elsif params[:title]
-      @tasks = Task.where('title LIKE ?', "%#{params[:title]}%")
+      @stauts = params[:status].to_i
+      @tasks = Task.status_search(@status)
+    elsif params[:title] && params[:status].blank?
+       @tasks = Task.title_search(params[:title])
+    elsif params[:title] && params[:status]
+       @status = params[:status].to_i
+       @tasks = Task.title_search(params[:title]).status_search(@status)
     else
       @tasks = Task.all.order(created_at: :desc)
     end
@@ -49,26 +56,13 @@ class TasksController < ApplicationController
   end
 
   private
-    def set_task
-      @task = Task.find(params[:id])
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :content, :deadline, :priority, :status, :user_id)
     end
 
-    # def order_string
-    #   return 'created_at DESC' unless params.key?(:order)
-    #   order_params.to_h.map { |key, val| "#{key} #{val.upcase}" }.join(',')
-    # end
-    #
-    # def prepare_search_attr
-    #   @search_attr = { title: '' }
-    #   @search_attr = task_params.delete_if { |_key, val| val.blank? } if params.key?(:task)
-    # end
-
-    def task_params
-      params.require(:task).permit(:title, :content, :deadline, :priority, :status, :user_id)
-    end
-
-    # def order_params
-    #   params.require(:order).permit(:deadline, :priority)
-    # end
 
 end
