f38b23ac978b76121a54fd0ca2060933979dbe58
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-03-06 17:14:20 +0900

    sessionのコントローラ生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 4 }, uniqueness: true
   validates :password, presence: true, length: { minimum: 7 }
-  has_secure_password
 
   has_many :tasks, dependent: :destroy
 
