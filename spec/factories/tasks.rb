49dbb5ed2c5d940314c3e3a366367f091ccf970e
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-03-06 21:57:37 +0900

    テストの修正完了

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,19 +1,75 @@
 FactoryBot.define do
-  factory :task do
-    # 下記の内容は実際に作成するカラム名に合わせて変更してください
-    title { 'test_title' }
+  factory :task, class: Task do
+    title { 'test_task' }
     content { 'test_content' }
-    deadline { "2020-01-02" }
-    priority { "test_priority" }
-    status { "test_status" }
+    priority { '高' }
+    status { '未着手' }
+    deadline { "2022-03-09" }
+    # user_id { 10 }
   end
 
-  factory :new_task, class: Task do
-    # 下記の内容は実際に作成するカラム名に合わせて変更してください
-    title { 'test_title2' }
+  factory :second_task, class: Task do
+    title { 'test_task2' }
     content { 'test_content2' }
-    deadline { "2020-01-02" }
-    priority { "test_priority2" }
-    status { "test_status2" }
+    priority { '中' }
+    status { '着手中' }
+    deadline { "2022-01-05" }
+    # user_id { 10 }
+  end
+
+  factory :third_task, class: Task do
+    title { 'test_task2' }
+    content { 'test_content2' }
+    priority { '中' }
+    status { '着手中' }
+    deadline { "2022-12-09" }
+    # user_id { 20 }
+  end
+
+  factory :search_task_01, class: Task do
+    title { '検索' }
+    content { '検索' }
+    priority { '低' }
+    status { '未着手' }
+    deadline { "2021-10-09" }
+    # user_id { 20 }
+  end
+
+  factory :search_task_02, class: Task do
+    title { 'ケンサク' }
+    content { 'ケンサク' }
+    priority { '高' }
+    status { '未着手' }
+    deadline { "2025-03-18" }
+    # user_id { 20 }
+  end
+
+  factory :search_task_03, class: Task do
+    title { 'けんさく' }
+    content { 'けんさく' }
+    priority { '低' }
+    status { '完了' }
+    deadline { "2024-03-26" }
+    # user_id { 30 }
   end
 end
+
+# FactoryBot.define do
+#   factory :task do
+#     # 下記の内容は実際に作成するカラム名に合わせて変更してください
+#     title { 'test_title' }
+#     content { 'test_content' }
+#     deadline { "2020-01-02" }
+#     priority { "test_priority" }
+#     status { "test_status" }
+#   end
+#
+#   factory :new_task, class: Task do
+#     # 下記の内容は実際に作成するカラム名に合わせて変更してください
+#     title { 'test_title2' }
+#     content { 'test_content2' }
+#     deadline { "2020-01-02" }
+#     priority { "test_priority2" }
+#     status { "test_status2" }
+#   end
+# end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,18 +1,63 @@
 require 'rails_helper'
-require 'byebug'
 
 RSpec.describe 'タスク管理機能', type: :system do
-  # before do
-  #   FactoryBot.create(:task)
-  #   FactoryBot.create(:new_task)
-  # end
+  before do
+    FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
+    FactoryBot.create(:search_task_03)
+  end
 
   describe 'タスク一覧画面' do
     context 'タスクを作成した場合' do
       it '作成済みのタスクが表示されること' do
+        visit tasks_path
+        expect(page).to have_content 'test_task'
+        expect(page).to have_content 'test_content'
+        expect(page).to have_content '高'
+        expect(page).to have_content '未着手'
+        expect(page).to have_content '2022-03-09'
+      end
+    end
+
+    context '複数のタスクを作成した場合' do
+      it 'タスクが作成日時の降順に並んでいること' do
         task = FactoryBot.create(:task, title: 'task')
         visit tasks_path
-        expect(page).to have_content 'task'
+        task_list = page.all('tr')
+        expect(task_list[1]).to have_content 'task'
+      end
+    end
+  end
+
+  describe 'ソート・検索機能' do
+    context '検索・ソート機能' do
+      it 'タイトルのみの検索結果が出ること' do
+        visit tasks_path
+        fill_in 'task[title]', with: 'test_task'
+        click_on '検索はコチラ'
+        title = page.all('tr')
+        expect(title[1]).to have_content 'test_task'
+        expect(title[2]).not_to have_content 'テストけんさく'
+      end
+
+      it 'ステータスのみ検索結果に出ること' do
+        visit tasks_path
+        select '未着手', from: 'task[status]'
+        click_on '検索はコチラ'
+        title = page.all('tr')
+        expect(title[1]).to have_content '未着手'
+        expect(title[2]).to have_content ''
+      end
+
+      it 'タイトル・ステータス共に検索して1件' do
+        visit tasks_path
+        fill_in 'task[title]', with: 'task'
+        click_on '検索はコチラ'
+        select '着手中', from: 'task[status]'
+        click_on '検索はコチラ'
+        title = page.all('tr')
+        expect(title[1]).to have_content 'test_task2', '着手中'
+        expect(title[2]).to have_content ''
       end
     end
   end
@@ -23,41 +68,15 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit new_task_path
         fill_in 'task_title', with: 'test_title'
         fill_in 'task_content', with: 'test_content'
-        fill_in 'task_deadline', with: Date.new(2020, 12 ,12)
-        select '高', from: 'task_priority'
-        select '未着手', from: 'task_status'
+        fill_in 'task_deadline', with: Date.new(2020, 11 ,30)
+        select "高", from: "task_priority"
+        select "未着手", from: "task_status"
         click_on '登録する'
         expect(page).to have_content 'test_title'
         expect(page).to have_content 'test_content'
-        expect(page).to have_content "2020-12-12"
+        expect(page).to have_content '2020-11-30'
         expect(page).to have_content "高"
         expect(page).to have_content "未着手"
-      end
-    end
-  end
-
-  describe 'タスク詳細画面' do
-     context '任意のタスク詳細画面に遷移した場合' do
-       it '該当タスクの内容が表示されたページに遷移すること' do
-         task = Task.create(title: 'test_title', content: 'test_content', deadline: "2020-12-12", priority: '高', status: '未着手')
-         visit task_path(task)
-         visit task_path(task.id)
-         expect(page).to have_content 'test_title'
-         expect(page).to have_content 'test_content'
-         expect(page).to have_content "2020-12-12"
-         expect(page).to have_content '高'
-         expect(page).to have_content '未着手'
-       end
-     end
-  end
-
-  describe 'タスク一覧画面' do
-    context '複数のタスクを作成した場合' do
-      it 'タスクが作成日時の降順に並んでいること' do
-        new_task = FactoryBot.create(:task, title: 'new_task')
-        visit tasks_path
-        task_list = page.all('tr')
-        expect(task_list[1]).to have_content 'new_task'
       end
     end
   end
@@ -70,18 +89,6 @@ RSpec.describe 'タスク管理機能', type: :system do
         click_on '【完了期限でソート】'
         click_on '【完了期限でソート】', match: :first
         expect(page).to have_content '超過'
-      end
-    end
-  end
-
-  describe "タスク一覧画面" do
-    context '優先順位でソートするボタンを押した場合' do
-      it '優先順位の降順に並んでいること' do
-        @task4 = FactoryBot.create(:task, title: "優先順位通りに並ぶ？", content: 'どうかな？？', status: '着手中', priority: '高')
-        visit tasks_path
-        click_on '【優先順位でソート】'
-        click_on '【優先順位でソート】', match: :first
-        expect(page).to have_content '優先順位'
       end
     end
   end
