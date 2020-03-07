ee1ff71ef9ece110a508063fec8969270fe4488e
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-03-08 00:58:26 +0900

    adminモデルの作成と編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 4 }, uniqueness: true
-  validates :password, presence: true, length: { minimum: 7 }
+  validates :email, presence: true, length: { maximum: 200 }, uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+  validates :password_digest, presence: true, length: { minimum: 7 }
 
+  before_validation { email.downcase! }
+  before_destroy :do_not_destroy_last_one_admin
+  has_secure_password
   has_many :tasks, dependent: :destroy
 
 end
